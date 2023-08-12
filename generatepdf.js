01123363982a9c72b1da615b7a4491ef76408b8c
Anand Chandrakant Bohara <aanandbohara@gmail.com>
2023-08-12 22:39:49 +0530

    Add files via upload

---
+++ generatepdf.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const jsPDF = require('jspdf');
+require('jspdf-autotable');
+
+function saveToPdf(failedAssertions, outputPath, xmlFileName) {
+  const doc = new jsPDF.jsPDF();
+  const timestamp = new Date().toISOString().replace(/:/g, '-');
+  const fileName = `Auto_Analysis_for_${xmlFileName.replace('.xml', '')}_${timestamp}.pdf`;
+  const tableData = failedAssertions.map((assertion, index) => [
+    index + 1,
+    assertion.TestSuiteName,
+    assertion.TestName,
+    assertion.FailureMessage,
+  ]);
+  const headerText = `Auto Analysis for ${xmlFileName.replace('.xml', '')}`;
+
+  doc.setFontSize(16);
+  doc.setTextColor(0, 0, 0);
+  doc.text(headerText, doc.internal.pageSize.getWidth() / 2, 10, { align: 'center' });
+
+  doc.autoTable({
+    head: [['#', 'Request Name', 'Failed Assertion', 'Failure Message']],
+    body: tableData,
+    startY: 20,
+    columnStyles: {
+      1: { cellWidth: 25 },
+      2: { cellWidth: 35 },
+    },
+    didDrawPage: function (data) {
+      // Add footer
+      const totalPages = doc.internal.getNumberOfPages();
+      const currentPage = data.pageNumber;
+      const footerText = `Page ${currentPage} of ${totalPages}`;
+      doc.setFontSize(8);
+      doc.text(footerText, doc.internal.pageSize.getWidth() / 2, doc.internal.pageSize.getHeight() - 10, { align: 'center' });
+    },
+  });
+
+  doc.save(path.join(outputPath, fileName));
+  console.log(`Results saved successfully to ${path.join(outputPath, fileName)}`);
+}
+
+
+module.exports = { saveToPdf };
+
---
+++ xmlprocessing.js
@@ -0,0 +1,41 @@
+const { DOMParser } = require('xmldom');
+
+function processXml(xmlContent) {
+  try {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(xmlContent, 'text/xml');
+    
+    const testSuites = xmlDoc.getElementsByTagName('testsuite');
+    const failedAssertions = [];
+
+    for (let i = 0; i < testSuites.length; i++) {
+      const testSuite = testSuites[i];
+      const testSuiteName = testSuite.getAttribute('name');
+      const testCases = testSuite.getElementsByTagName('testcase');
+
+      for (let j = 0; j < testCases.length; j++) {
+        const testCase = testCases[j];
+        const testName = testCase.getAttribute('name');
+        const failureElement = testCase.getElementsByTagName('failure')[0];
+
+        if (failureElement) {
+          const failureMessage = failureElement.getAttribute('message');
+          const assertionDetails = {
+            TestSuiteName: testSuiteName,
+            TestName: testName,
+            FailureMessage: failureMessage,
+          };
+          failedAssertions.push(assertionDetails);
+        }
+      }
+    }
+
+    return failedAssertions;
+  } catch (error) {
+    console.log('Error occurred while processing the XML content:', error.message);
+    throw error;
+  }
+}
+
+module.exports = { processXml };
+
